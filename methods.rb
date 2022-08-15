fcc751f3e93d08a78838b23979d69dbd3d496d61
Francis <francisodhiambo2@gmail.com>
2022-08-16 00:27:17 +0300

    commit tree 5a0432157c326544fa5aa692685a802aa65ea82b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+def greet(name = "Francis")
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+
+def add (num1, num2)
+    return num1 + num2
+
+end
+
+def halve(number)
+    if number.class == Integer
+      return number/2
+    end
+
+    return nil
+  end 
+
+  
